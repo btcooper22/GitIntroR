572b19247d37eb5beefd16cf50d1a05735693c7d
Ben Cooper <ben.cooper.226@gmail.com>
2021-08-11 15:36:54 +0100

    Changed B to categorical and added summary stats

--- scripts/data_generation.R
+++ scripts/data_generation.R
@@ -3,15 +3,24 @@ require(ggplot2)
 require(tibble)
 require(readr)
 require(tidyr)
+require(janitor)
 
 # Create some data
 A <- rnorm(1000)
-B <- rnorm(1000, 2)
+B <- rep(c("Class 1", "Class 2"), 500)
 C <- rlnorm(1000)
 
 # Add into data frame
 df <- tibble(A, B, C) %>% 
   mutate(D = log(C))
+
+# Tables of summary statistics
+tabyl(df, B)
+df %>% 
+  group_by(B) %>% 
+  summarise(meanA = mean(A),
+            meanC = mean(C),
+            meanD = mean(D))
 
 # Write data to file
 if(!dir.exists("data"))
@@ -22,7 +31,7 @@ write_csv(df, "data/raw_data.csv")
 
 # Transform
 df %>% 
-  pivot_longer(1:4) %>% 
+  pivot_longer(c(1,3,4)) %>% 
   # Plot
   ggplot(aes(x = value))+
   geom_density(aes(fill = name),
